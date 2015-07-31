2c0f97db4d02bead33c6619dab576b74871fc1e2
BogdanVM <bogdanvm98@gmail.com>
2015-07-31 14:17:50 +0300

    Update Votare.php

--- Surse/Votare.php
+++ Surse/Votare.php
@@ -30,31 +30,31 @@
                    mysqli_stmt_bind_param($state, "ss", $_SESSION['Nume'], $_SESSION['parola']);
                    mysqli_stmt_execute($state);
                    mysqli_close($conn);
-                    //echo "<script>location.assign('index.php')</script>";
+                    
           }
           else{     
                $con = mysqli_connect("localhost","root","root","Alegatori") or die("Error " . mysqli_error($link)); 
                $query = "SELECT * FROM Candidati";
                 $res = mysqli_query($con, $query);
+              $id = 1;
               while($row = mysqli_fetch_assoc($res)){
-              if (isset($_POST[$row['Nume']])) {
+              if (isset($_POST[$id])) {
                    $nr_vot = $row['Voturi'] + 1;
                    $nume = $row['Nume'];
-                   //$qer = "UPDATE Candidati SET Voturi = ". $nr_vot . "WHERE Nume = ". $nume;
+                   
                    $qer = "UPDATE Candidati SET Voturi = ? WHERE Nume = ?";
                    $stmt = mysqli_prepare($con, $qer);
                    mysqli_stmt_bind_param($stmt, "is", $nr_vot, $nume);
                    mysqli_stmt_execute($stmt);
-                   //mysqli_close($con);
                    
-                   //$conn = mysqli_connect("localhost","root","root","Alegatori") or die("Error " . mysqli_error($link));
                    $msql = "DELETE FROM Conturi WHERE Nume = ? AND Parola = ?";
                    $state = mysqli_prepare($con, $msql);
                    mysqli_stmt_bind_param($state, "ss", $_SESSION['Nume'], $_SESSION['parola']);
                    mysqli_stmt_execute($state);
                    mysqli_close($con);
-                    //echo "<script>location.assign('index.php')</script>";
+                    
               }
+            $id = $id + 1;
              }
           }
         
